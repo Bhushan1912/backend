027a3a7697502e886b27a5ddc6569057b9fdfac0
bhushan <mude.bhushan@gmail.com>
2023-05-23 13:24:40 +0530

    make changes in env file

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const userRouter = require('./routes/user');
 const app = express();
 app.use(express.json());
 
+const PORT = process.env.PORT;
 app.use('/api/user',userRouter);
 
 app.get('/about', (req,res) =>{
     res.send('<h1>Hello i am from your About</h1>');
     });
-app.listen(process.env.PORT,() =>{
-    console.log('port is listening on 8080');
+app.listen(8000,() =>{
+    console.log('port is listening on', {PORT});
 });
