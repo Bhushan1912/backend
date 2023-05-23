85a6e05f19acc5fc2a0d7988f38b99f0d48547eb
bhushan <mude.bhushan@gmail.com>
2023-05-23 13:29:00 +0530

    changes in env files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 require('./db');
+require('dotenv').config();
 const userRouter = require('./routes/user');
 const app = express();
 app.use(express.json());
@@ -9,6 +10,6 @@ app.use('/api/user',userRouter);
 app.get('/about', (req,res) =>{
     res.send('<h1>Hello i am from your About</h1>');
     });
-app.listen(8002,() =>{
+app.listen(process.env.PORT,() =>{
     console.log('port is listening on 8080');
 });
--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/review_app')
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("DB is connected");
 })
